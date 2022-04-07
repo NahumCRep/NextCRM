5a89c42e98ca7ce4ff486f40a04fc80bcbac787d
Nahum Casco <nahumcasco.v@gmail.com>
2022-04-07 17:13:25 -0500

    login components

---
+++ components/Input.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { MdError } from 'react-icons/md'
+
+const Input = ({label, name, type, placeholder, onChange, value, defaultValue, errorMessage}) => {
+  return (
+    <div className='flex flex-col mt-4'>
+        {label && <label className='font-semibold px-1 text-sm' htmlFor={name}>{label}</label>}
+        <input name={name} type={type} value={value} onChange={onChange} placeholder={placeholder} className='text-sm font-medium placeholder:text-slate-500 dark:placeholder:text-gray-300  h-9 mt-1 px-2 rounded-md bg-palette-gray dark:bg-palette-darkgray outline-none border-2 border-transparent focus:border-palette-blue dark:focus:border-palette-yellow' />
+        {errorMessage && <p className='text-red-700 flex items-center gap-1 px-1 text-sm font-medium mt-1'><MdError /> {errorMessage}</p>}
+    </div>
+  )
+}
+
+export default Input
