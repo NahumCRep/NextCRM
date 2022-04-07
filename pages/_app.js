6f6884991457c676eb0031647bcaca78f90b13df
Nahum Casco <nahumcasco.v@gmail.com>
2022-04-07 17:15:08 -0500

    theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import { SessionProvider } from 'next-auth/react'
+import { ThemeProvider } from 'next-themes'
+import Page from '../components/templates/Page'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <SessionProvider session={pageProps.session}>
+    <ThemeProvider attribute='class'>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ThemeProvider>
+  </SessionProvider>
 }
 
 export default MyApp
