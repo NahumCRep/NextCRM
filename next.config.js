71340c47ab7beff7785053c5920148b137ca2137
Nahum Casco <nahumcasco.v@gmail.com>
2022-04-12 00:34:07 -0500

    images extensions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    domains:[
+      "lh3.googleusercontent.com",
+      "avatars.githubusercontent.com",
+      "images.unsplash.com",
+      "images.pexels.com"
+    ]
+  }
 }
 
 module.exports = nextConfig
